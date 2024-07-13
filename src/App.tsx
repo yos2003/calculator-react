98516060cbb70754e2689b526b4295f7c5ec40a2
yos2003 <158718649+yos2003@users.noreply.github.com>
2024-07-13 16:36:46 +0100

    btns

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,25 @@ function App() {
       <div className="box">
         <div className="header"></div>
         <div className="container">
-          <Btn avg="avg">1</Btn>
-          <Btn avg="avg">2</Btn>
-          <Btn avg="avg">3</Btn>
-          <Btn avg="avg">4</Btn>
-          <Btn avg="avg">5</Btn>
-          <Btn avg="avg">6</Btn>
+          <Btn avg="avg">C</Btn>
+          <Btn avg="avg">+-</Btn>
+          <Btn avg="avg">%</Btn>
+          <Btn avg="avg">/</Btn>
           <Btn avg="avg">7</Btn>
           <Btn avg="avg">8</Btn>
           <Btn avg="avg">9</Btn>
-          <Btn avg="avg">10</Btn>
-          <Btn avg="avg">11</Btn>
-          <Btn avg="avg">12</Btn>
-          <Btn avg="avg">13</Btn>
-          <Btn avg="avg">14</Btn>
-          <Btn avg="avg">15</Btn>
-          <Btn avg="avg">16</Btn>
-          <Btn avg="avg">17</Btn>
-          <Btn avg="avg">18</Btn>
-          <Btn avg="horizontal">19</Btn>
+          <Btn avg="avg">X</Btn>
+          <Btn avg="avg">4</Btn>
+          <Btn avg="avg">5</Btn>
+          <Btn avg="avg">6</Btn>
+          <Btn avg="avg">-</Btn>
+          <Btn avg="avg">1</Btn>
+          <Btn avg="avg">2</Btn>
+          <Btn avg="avg">3</Btn>
+          <Btn avg="avg">+</Btn>
+          <Btn avg="avg">0</Btn>
+          <Btn avg="avg">.</Btn>
+          <Btn avg="horizontal">=</Btn>
 
         </div>
       </div>
